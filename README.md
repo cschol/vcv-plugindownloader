ae925cd722052f0bb2aafe05f5043b4b7ba1c553
Christoph Scholtes <cschol2112@gmail.com>
2018-01-14 23:28:20 -0700

    Add fallback to build modules from source if no binary release is available. Add option for specifying list of plugins to download.

--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 
 This simple script parses the plugin information provided by the VCV Rack [community repository](https://github.com/VCVRack/community)
 and download the **binary release** versions of plugins available for the specified target platform (Windows, MacOS, Linux).
+If a binary release is not available, an option can be provided on the command line to fall back on building the plugin from source.
 
 The script
 
 - clones/updates the `community` repository for access to the latest plugin information.
-- downloads the archive of **binary releases** (**not** source!), verifies the `sha256`, and extracts the archive.
+- downloads the archive of **binary releases**, verifies the `sha256`, and extracts the archive.
 - discards any archives that fail `sha256` verification (or encounter any other error) and does **not** extract the archive.
 - only downloads archives that are not already present in the `downloads` directory (verified via `sha256`).
 - only updates the local version of the plugin if a new version was downloaded.
+- can fall back to cloning and building the plugin from source (via command line option). This includes a `git pull` to get the latest version.
 
 ## Prerequisites
 
@@ -28,17 +30,41 @@ cd ~/src/Rack/plugins
 ./vcv-plugindownloader/vcv-plugindownloader.py win
 ```
 
-Valid target platforms are: `win`, `mac`, `lin`.
-
 **NOTE:** The script always assumes the **current working directory** is the `plugins` directory! 
 
-### Specifying which plugins to download
+### Arguments
 
-The *optional* `-l` (or `--list`) argument allows specifying individual plugins to download in a *whitespace-separated* list:
+- The *required* platform argument selects the platform, for which plugins are downloaded.
+
+Valid platforms are: `win`, `mac`, `lin`.
+
+- The *optional* `-l` (or `--list`) argument allows specifying individual plugins to download in a *whitespace-separated* list:
 
 ```
 ./vcv-plugindownloader/vcv-plugindownloader.py win -l AudibleInstruments Grayscale
 ```
+
+- The *optional* `-s` (or `--source`) argument attempts to fall back on cloning and building the plugin from source:
+
+```
+./vcv-plugindownloader/vcv-plugindownloader.py win -s
+./vcv-plugindownloader/vcv-plugindownloader.py win -s -l Fundamental
+```
+
+- The *optional* `-j` (or `--jobs`) argument specifies number of jobs to pass to `make` with the `-j` argument:
+
+```
+./vcv-plugindownloader/vcv-plugindownloader.py win -s -l Fundamental -j 8
+```
+
+Obviously only has an effect when `-s` is specified.
+
+### Notes
+
+For certain modules (e.g. Fundamental), a git `branch` or `tag` needs to be checked out for the build to succeed
+(`master` is not compatible with the currently release tag).
+This `branch` or `tag` is hard-coded in the script and will be updated as required. Note, that plugins that are
+pinned like that will not be updated automatically as other modules that just build from `HEAD` of the `master` branch.
 
 ## Supported platforms
 
--- vcv-plugindownloader.py
+++ vcv-plugindownloader.py
@@ -12,14 +12,19 @@ import subprocess
 import argparse
 import traceback
 
-
-__version__ = "1.1.0"
+__version__ = "2.0.0"
 
 
 COMMUNITY_REPO = "https://github.com/VCVRack/community.git"
 COMMUNITY_REPO_DIR = os.path.join(os.getcwd(), "community")
 DOWNLOAD_DIR = os.path.join(os.getcwd(), "downloads")
 FAILED_CHECKSUM_DIR = os.path.join(DOWNLOAD_DIR, "failed_checksum")
+
+
+# Certain plugins require special branches, tags, or shas to be checked out to build successfully.
+PLUGIN_COMMITTISH_MAP = {
+    "Fundamental": "v0.5.1"
+}
 
 
 def download_from_url(url, target_path=os.getcwd()):
@@ -43,8 +48,40 @@ def parse_args(argv):
 
     parser.add_argument("platform", help="platform to download plugins for", type=str, choices=["win", "mac", "lin"])
     parser.add_argument("-l", "--list", nargs='+', help="list of plugins to download (white-space separated)")
+    parser.add_argument("-s", "--source", action='store_true', help="attempt to build plugins from source if binary release is not available")
+    parser.add_argument("-j", "--jobs", type=int, help="number of jobs to pass to make command via -j option", default=1)
 
     return parser.parse_args()
+
+
+def get_source_dir(plugin_name):
+    return os.path.join(os.getcwd(), plugin_name.replace(" ", "_")+".git")
+
+
+def clone_source(plugin_name, git_repo_url, committish=None):
+    try:
+        subprocess.check_call(["git", "clone", git_repo_url, os.path.basename(get_source_dir(plugin_name))], cwd=os.path.join(os.getcwd()))
+        if committish:
+            subprocess.check_call(["git", "checkout", committish], cwd=get_source_dir(plugin_name))
+    except Exception as e:
+        print("[%s] ERROR: Failed to clone source" % plugin_name)
+        raise e
+
+
+def update_source(plugin_name, git_repo_url):
+    try:
+        subprocess.check_call(["git", "pull", "origin", "master"], cwd=get_source_dir(plugin_name))
+    except Exception as e:
+        print("[%s] ERROR: Failed to update source" % plugin_name)
+        raise e
+
+
+def build_source(plugin_name, num_jobs=1):
+    try:
+        subprocess.check_call(["make", "-j%s" % num_jobs], cwd=get_source_dir(plugin_name))
+    except Exception as e:
+        print("[%s] ERROR: Failed to build from source" % plugin_name)
+        raise e
 
 
 def main(argv=None):
@@ -53,20 +90,27 @@ def main(argv=None):
 
     platform = args.platform
     plugin_list = args.list
-
+    build_from_source = args.source
+    num_jobs = args.jobs
+   
     PLATFORM_STRING = {"win": "Windows", "mac": "MacOS", "lin": "Linux"}
 
     print("VCV Plugin Downloader v%s" % __version__)
     print("Platform: %s" % PLATFORM_STRING[platform])
 
+    error_list = []
+
     try:
+        # 'community' repository contains all of the plugin information.
         print("Updating community repository...")
         if not os.path.exists(COMMUNITY_REPO_DIR):
             subprocess.check_call(["git", "clone", COMMUNITY_REPO]  )
         else:
             subprocess.check_call(["git", "pull"], cwd=COMMUNITY_REPO_DIR)
         
+        # Build a list of plugins to download.
         plugins_json = []
+        # Any plugins specified on command line?
         if plugin_list:
             for l in plugin_list:
                 p = glob.glob(os.path.join(COMMUNITY_REPO_DIR, "plugins/%s.json" % l))
@@ -75,91 +119,111 @@ def main(argv=None):
                 else:
                     plugins_json.append(p[0])
         else:
+            # Grab all .json files in the plugins directory.
             plugins_json = glob.glob(os.path.join(COMMUNITY_REPO_DIR, "plugins/*.json"))
 
         if not plugins_json:
-            print("ERROR: No valid plugins found in community repository")
+            print("ERROR: No valid plugins found")
             return 1
 
+        # Housekeeping
         if not os.path.exists(DOWNLOAD_DIR):
             os.mkdir(DOWNLOAD_DIR)
         if os.path.exists(FAILED_CHECKSUM_DIR):
             shutil.rmtree(FAILED_CHECKSUM_DIR)
 
+        #
+        # Process all plugins in our assemble list.
+        #
         for plugin in plugins_json:
             with open(plugin) as json_data:
                 pj = json.load(json_data)
 
-                name = pj['name']
+                slug = pj['slug']
                 version = pj['version'] if "version" in pj.keys() else "UNKNOWN VERSION"
 
-                print("[%s] Version %s" % (name, version))
+                print("[%s] Version %s" % (slug, version))
 
-                download = False
-                extract = False
+                download = True
+                extract = True
+                build = build_from_source
 
+                #
+                # Binary release available to download?
+                #
                 if "downloads" in pj:
+                    #
+                    # Binary release available for the selected platform?
+                    #
                     if platform in pj["downloads"].keys():
                         url = pj["downloads"][platform]["download"]
                         file_name = os.path.basename(url).split('?')[0]
                         sha256 = pj["downloads"][platform]["sha256"] if "sha256" in pj["downloads"][platform].keys() else None
-                        
                         download_file = os.path.join(DOWNLOAD_DIR, file_name)
                             
+                        #
+                        # If downloaded artifact exists already, check if it is the latest version (based on SHA256)
+                        #
                         if os.path.exists(download_file):
                             if not sha256:
-                                print("[%s] ERROR: Missing SHA256 checksum in .json file! Skipping module." % name)
+                                print("[%s] ERROR: Missing SHA256 checksum in .json file! Skipping module." % slug)
                                 download = False
 
-                            # If there is a checksum mismatch, download the archive.
+                            # If there is a checksum mismatch, there must be a new version 
+                            # (or the current one is corrupt). Download the archive.
                             if sha256 != hash_sha256(download_file):
                                 os.remove(download_file)
-                                download = True
                             else:
-                                print("[%s] Already at newest version. Skipping download." % name)
+                                print("[%s] Already at newest version. Skipping download." % slug)
                                 download = False
-                        else:
-                            download = True
-
+                        
+                        #
+                        # Do we need to download a (potentially newer) version of the plugin?
+                        #
                         if download:
                             try:
-                                print("[%s] Downloading version %s..." % (name, version), end='', flush=True)
+                                print("[%s] Downloading version %s..." % (slug, version), end='', flush=True)
                                 download_from_url(url, DOWNLOAD_DIR)
                             except Exception as e:
-                                print("ERROR: Failed to download archive for %s: %s" % (name, e))
+                                print("ERROR: Failed to download archive for %s: %s" % (slug, e))
+                                error_list.append(slug)
                                 continue
 
                             def move_failed_file():
                                 if not os.path.exists(FAILED_CHECKSUM_DIR):
                                     os.mkdir(FAILED_CHECKSUM_DIR)
                                 shutil.move(download_file, os.path.join(FAILED_CHECKSUM_DIR, file_name))
+                                error_list.append(slug)
 
                             if not sha256:
                                 print("ERROR: Missing SHA256 checksum in .json file!")
                                 move_failed_file()
                                 continue
 
-                            if sha256 != hash_sha256(download_file):
-                                print("ERROR: Checksum verification failed!")
+                            checksum = hash_sha256(download_file)
+                            if sha256 != checksum:
+                                print("ERROR: Checksum verification failed")
+                                print("expected: %s" % sha256)
+                                print("actual:   %s" % checksum)
                                 move_failed_file()
                                 continue
                             else:
                                 print("OK")
 
+                        #
+                        # Update local (extracted) version of the plugin?
+                        #
                         module_dir = os.path.join(os.getcwd(), zipfile.ZipFile(download_file).namelist()[0])
                         
                         if os.path.exists(module_dir):
                             # If we downloaded the module, replace the current version with the new one.
                             if download:
                                 shutil.rmtree(module_dir)
-                                extract = True
                             else:
                                 extract = False
-                        else:
-                            extract = True
 
                         if extract:
-                            print("[%s] Extracting..." % name, end='', flush=True)
+                            print("[%s] Extracting..." % slug, end='', flush=True)
                             try:
                                 with zipfile.ZipFile(download_file) as z:
                                     z.extractall(os.getcwd())
@@ -168,10 +232,56 @@ def main(argv=None):
                                 print("ERROR: Failed to extract module: %s" % e)
                                 if os.path.exists(module_dir):
                                     shutil.rmtree(module_dir)
+                                error_list.append(slug)
+                                continue
+
+                        # Plugin downloaded and extracted successfully. No need to build from source.
+                        build = False
+                    
+                    #
+                    # No binary release available for selected platform
+                    #
                     else:
-                        print("[%s] WARNING: Binary archive download for platform %s not available" % (name, PLATFORM_STRING[platform]))
+                        print("[%s] WARNING: Binary archive download for platform %s not available" % (slug, PLATFORM_STRING[platform]))
+
+                #
+                # No binary release download exists
+                #
                 else:
-                    print("[%s] WARNING: No binary archive downloads available in repository" % name)
+                    print("[%s] WARNING: No binary archive downloads available in repository" % slug)
+
+                #
+                # Build plugin from source?
+                #
+                if build:
+                    source_url = pj["source"] if "source" in pj.keys() else None
+                    
+                    if source_url:
+                        committish = PLUGIN_COMMITTISH_MAP[slug] if slug in PLUGIN_COMMITTISH_MAP.keys() else None
+                        
+                        if not os.path.exists(get_source_dir(slug)):
+                            print("[%s] Cloning plugin source..." % slug)
+                            clone_source(slug, source_url, committish)
+                        else:
+                            if not committish:
+                                print("[%s] Updating plugin source..." % slug)
+                                update_source(slug, source_url)
+                            else:
+                                print("[%s] Repository pinned to version %s. Not updating." % (slug, committish))
+                        
+                        print("[%s] Building plugin..." % slug)
+                        try:
+                            build_source(slug, num_jobs)
+                        except Exception as e:
+                            error_list.append(slug)
+                    else:
+                        print("[%s] No source URL specified in JSON file. Skipping." % slug)
+        
+        if error_list:
+            print("")
+            print("PLUGINS WITH ERRORS: %s" % " ".join(error_list))
+            return 1
+
         return 0
 
     except Exception as e:
